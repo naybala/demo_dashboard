191ca1039bbc9e3e46f33cb7ede5d4904426485a
naybala <naybalaismylife.nbl@gmail.com>
2026-02-14 00:14:28 +0700

    feat: Hide duplicate voucher numbers and remove the iteration column from the daily income table.

--- resources/views/admin/daily-income/index.blade.php
+++ resources/views/admin/daily-income/index.blade.php
@@ -6,15 +6,19 @@
                 <x-common.create-button route="daily-incomes.create" permission="create daily-incomes" />
             </div>
             <x-table.wrapper>
-                <x-table.header :fields="['#', 'date', 'voucher_no', 'own_product', 'amount', 'price', 'investment', 'profit', 'unit']" />
+                <x-table.header :fields="['date', 'voucher_no', 'own_product', 'amount', 'price', 'investment', 'profit', 'unit']" />
                 <x-table.body :checkData="count($data['data'])>0">
+                    @php $lastVoucher = null; @endphp
                     @foreach ($data['data'] as $record)
                         <x-table.body-row>
-                            <x-table.body-column :field="$loop->iteration + ($data['meta']['from'] - 1)" />
                             <x-table.body-column :field="$record['date']" limit="20" />
-                            <x-table.body-column :field="$record['voucher_no'] ?? '-'" limit="20" />
+                            @if ($lastVoucher != $record['voucher_no'])
+                                <x-table.body-column :field="$record['voucher_no'] ?? '-'" limit="20" />
+                                @php $lastVoucher = $record['voucher_no']; @endphp
+                            @else
+                                <x-table.body-column field="" limit="20" />
+                            @endif
                             <x-table.body-column :field="$record['own_product']" limit="20" />
-
                             <x-table.body-column :field="$record['amount']" limit="20" />
                             <x-table.body-column :field="$record['price']" limit="20" />
                             <x-table.body-column :field="$record['investment']" limit="20" />
