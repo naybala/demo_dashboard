d50a25e40914b1dac6c51f10a04417608027cb95
naybala <naybalaismylife.nbl@gmail.com>
2026-03-04 12:36:14 +0700

    feat: Dynamically set dashboard page title and increase Inertia progress bar delay.

--- resources/js/Pages/Dashboard/Index.svelte
+++ resources/js/Pages/Dashboard/Index.svelte
@@ -72,6 +72,10 @@
   };
 </script>
 
+<svelte:head>
+  <title>{__("sidebar.dashboard")}</title>
+</svelte:head>
+
 <AdminLayout>
   <svelte:fragment slot="header">
     <h2
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import "../css/app.css";
 import { createInertiaApp } from "@inertiajs/svelte";
 createInertiaApp({
   progress: {
-    delay: 0,
+    delay: 250,
     color: "#004bfa",
     showSpinner: false,
   },
