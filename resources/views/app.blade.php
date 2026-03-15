b15374b9ae497d8380240af3dbaf773698ca2a60
naybala <naybalaismylife.nbl@gmail.com>
2026-03-15 16:34:11 +0700

    feat: add favicon to the application.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="icon" type="image/png" href="{{ asset('/images/logo.png') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
